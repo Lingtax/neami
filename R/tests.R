9ccd347c5abba25691775cee6c4e0e56cb914354
Mathew <mathewtyling@gmail.com>
2022-08-12 12:36:06 +1000

    We added a caption function for a t-test and did the documentation.

--- R/tests.R
+++ R/tests.R
@@ -15,4 +15,24 @@ paired_t_test <-  function(df, outcome, timeid) {
   x <- purrr::pluck(df, lvls[1])
   y <- purrr::pluck(df, lvls[2])
   t.test(x, y, paired = TRUE)
-  }
+}
+
+
+
+#' Wraps the output of a t-test into a pretty format
+#'
+#' @param ttest An S3 t-test model object
+#'
+#' @return A character string of the model summary
+#' @export
+#'
+#' @examples
+caption_ttest <- function(ttest) {
+
+  paste0('The difference between groups was ',
+         ifelse(ttest$p.value > .05, 'not ', ''),
+         'statistically significant (',
+         t_apa(ttest, print = FALSE),
+         ')')
+
+}
