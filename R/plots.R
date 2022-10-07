f29254e5293e05469540b472cd0ec12704a09077
Mathew <mathewtyling@gmail.com>
2022-10-07 12:44:51 +1100

    Added a neami_waffle function and pipe compatability

--- R/plots.R
+++ R/plots.R
@@ -22,3 +22,29 @@ paired_t_plot <-  function(data, x, y, group) {
     ggplot2::labs(x = "Timepoint", y = "Measure score") +
     neami::theme_neami()
 }
+
+
+
+#' Makes an opinionated waffleplot
+#'
+#' @param data a dataframe
+#' @param count_var a bare variable name indicating the column to be counted
+#' @param palette a ggplot2 compatable pallette
+#'
+#' @return a waffleplot
+#' @export
+#'
+#' @examples
+#' neami_waffle(mtcars, cyl)
+neami_waffle <- function(data, count_var, palette = "Set2") {
+
+  waf_dat <- data %>%
+    tidyr::drop_na({{count_var}}) %>%
+    dplyr::group_by({{count_var}}) %>%
+    dplyr::summarise(count=n()) %>%
+    dplyr::pull(count, name={{count_var}})
+
+  waffle::waffle(waf_dat,rows=5,xlab='1 Square = 1 Consumer',
+                 colors = RColorBrewer::brewer.pal(8, palette)[seq_along(waf_dat)])
+
+}
