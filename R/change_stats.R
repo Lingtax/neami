63046b9fbf34263c561ffef8f7926044d21bc62e
Mathew <mathewtyling@gmail.com>
2022-10-13 18:26:23 +1100

    Generalised reliable change calculator to work for k10 and ras-ds
    Lost ability to take user defined reliability and sd inputs

--- R/change_stats.R
+++ R/change_stats.R
@@ -12,24 +12,37 @@
 #' @param data a dataframe in wide format
 #' @param pre the column containing the pre-measure
 #' @param post the column containing a post measure
-#' @param pop_sd a revised estimate for the population standard deviation
-#' @param rel a revised estimate from the measure reliability
+#' @param measure the name of a supported measure ("ras", or "k10")
 #'
 #' @return
 #' @export
 #'
 #' @examples
 #' data <-  data.frame(person = 1:30, pre = rnorm(30, 30, 9.2), post = rnorm(30, 22, 9.2))
-#' add_k10_change(data, pre, post)
-add_k10_change <- function(data, pre, post, pop_sd = 9.2, rel = .92) {
+#' add_rel_change(data, pre, post, measure = "k10")
+add_rel_change <- function(data, pre, post, measure = NULL) {
 
 
-  crit = 1.96*pop_sd*sqrt(2)*sqrt(1-rel)
+  if (measure == "k10") {
+    # From Neami stats on 2022/10/7 n > 700 n = 2215
+    pop_sd <-  8.84
+    rel <-  .92
+  }
+
+  if (measure == "ras") {
+    # From Neami stats on 2022/10/13 n > 700
+    pop_sd <-  17.95
+    rel <-  .94
+
+  }
+
+
+  crit <-  1.96 * pop_sd * sqrt(2) * sqrt(1 - rel)
 
   data %>%
-    mutate(k10_change = {{post}} - {{pre}},
-           rel_k10_change = case_when(k10_change < -crit ~ "Improved",
-                                      k10_change > crit ~ "Worsened",
+    mutate(change = {{post}} - {{pre}},
+           rel_change = case_when(change < -crit ~ "Improved",
+                                      change > crit ~ "Worsened",
                                       TRUE ~ "Unchanged"
 
 
@@ -39,8 +52,7 @@ add_k10_change <- function(data, pre, post, pop_sd = 9.2, rel = .92) {
 
 
 
-
-#' Flags reliable changes to k10s in a long data frame
+#' Flags reliable changes to measures in a long data frame
 #'
 #' @param data a long data frame containing 2 observations per consumer
 #' @param name the unquoted column name containing timepoint names
@@ -53,14 +65,14 @@ add_k10_change <- function(data, pre, post, pop_sd = 9.2, rel = .92) {
 #' @examples
 #' data <-  data.frame(person = 1:30, pre = rnorm(30, 30, 9.2), post = rnorm(30, 22, 9.2))
 #' dl <- tidyr::pivot_longer(data,  pre:post)
-#' flag_k10_changes(dl, name, value, levels = c("pre", "post"))
-flag_k10_changes <-  function(data, name = name, value = value, levels = c("pre", "post")) {
+#' flag_rel_changes(dl, name, value, measure = "k10", levels = c("pre", "post"))
+flag_rel_changes <-  function(data, name = name, value = value, measure = NULL,  levels = c("pre", "post")) {
 
 
   data %>%
-  pivot_wider(names_from = {{name}}, values_from = {{value}}) %>%
-  add_k10_change(pre = .[levels[[1]]], post = .[levels[[2]]]) %>%
-    pivot_longer(cols = levels) %>%
-    unnest(all_of("k10_change")) %>%
-  rename(k10_change = matches(all_of(levels[[2]])))
+    tidyr::pivot_wider(names_from = {{name}}, values_from = {{value}}) %>%
+  add_rel_change(pre = .[levels[[1]]], post = .[levels[[2]]], measure = measure) %>%
+    tidyr::pivot_longer(cols = all_of(levels)) %>%
+    tidyr::unnest(all_of("change")) %>%
+    dplyr::rename(change = matches(all_of(levels[[2]])))
 }
