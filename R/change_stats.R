850f5ee7d380f818111d04145eb41185746dc761
Mathew <mathewtyling@gmail.com>
2025-06-20 16:40:26 +1000

    Add add_rasds_change and add_k5_change functions.

--- R/change_stats.R
+++ R/change_stats.R
@@ -43,8 +43,42 @@ add_k10_change <- function(data, pre, post, pop_sd = 9.2, rel = .92) {
 #' unrelated to a consumers' actual outcomes. Reliable change indices
 #' /url(https://ebmh.bmj.com/content/1/3/70) seek to place benchmarks on changes
 #' that are attributable to random noise and those that are more systematic.
-#' This calculation relies on an estimate of the standard deviation (ABS SD = 10, N = 10579) and of scale
-#' reliability (alpha = .91: https://rdcu.be/cW31L, neami estimate alpha = .92)
+#' The critical value in this function was provided by the department of health 2024-25
+#'
+#' @param data a dataframe in wide format
+#' @param pre the column containing the pre-measure
+#' @param post the column containing a post measure
+#' 
+#' @return the input dataframe with a raw change and a reliable change indicator appended
+#' @export
+#'
+#' @examples
+#' data <-  data.frame(person = 1:30, pre = rnorm(30, 15, 7), post = rnorm(30, 11, 7))
+#' add_k5_change(data, pre, post)
+add_k5_change <- function(data, pre, post) {
+  
+  
+  crit = 4.960261645 # From DoH PMHC - 2025
+  
+  dplyr::mutate(data, k5_change = {{post}} - {{pre}},
+                rel_k5_change = dplyr::case_when(k5_change < -crit ~ "Decreased",
+                                                  k5_change > crit ~ "Increased",
+                                                  TRUE ~ "Unchanged"
+                                                  
+                                                  
+                ))
+  
+}
+
+#' Calculates reliable change for ras-ds scores
+#'
+#' Simple differences in pre- and post- scores risks overinterpreting variation.
+#' Variation in outcome measures are to be expected due to uncontrolled factors
+#' unrelated to a consumers' actual outcomes. Reliable change indices
+#' /url(https://ebmh.bmj.com/content/1/3/70) seek to place benchmarks on changes
+#' that are attributable to random noise and those that are more systematic.
+#' This calculation relies on an estimate of the standard deviation (Neami 
+#' intake estimate 20, n ~= 900) and of scale  reliability (neami estimate alpha = .94)
 #'
 #' @param data a dataframe in wide format
 #' @param pre the column containing the pre-measure
@@ -56,23 +90,22 @@ add_k10_change <- function(data, pre, post, pop_sd = 9.2, rel = .92) {
 #' @export
 #'
 #' @examples
-#' data <-  data.frame(person = 1:30, pre = rnorm(30, 30, 9.2), post = rnorm(30, 22, 9.2))
-#' add_k10_change(data, pre, post)
-add_k10_change <- function(data, pre, post, pop_sd = 9.2, rel = .92) {
+#' data <-  data.frame(person = 1:30, pre = rnorm(30, 85, 20), post = rnorm(30, 115, 20))
+#' add_rasds_change(data, pre, post)
+add_rasds_change <- function(data, pre, post, pop_sd = 20, rel = .94) {
   
   
   crit = 1.96*pop_sd*sqrt(2)*sqrt(1-rel)
   
-  dplyr::mutate(data, k10_change = {{post}} - {{pre}},
-                rel_k10_change = dplyr::case_when(k10_change < -crit ~ "Decreased",
-                                                  k10_change > crit ~ "Increased",
+  dplyr::mutate(data, rasds_change = {{post}} - {{pre}},
+                rel_rasds_change = dplyr::case_when(rasds_change < -crit ~ "Decreased",
+                                                  rasds_change > crit ~ "Increased",
                                                   TRUE ~ "Unchanged"
                                                   
                                                   
                 ))
   
 }
-
 
 #' Flags reliable changes to k10s in a long data frame
 #'
