6887d38b83485745fa05f0ae9b07c104720e0237
Lingtax <mathewtyling@gmail.com>
2026-07-15 11:57:12 +1000

    Update the neami waffle to correct bug in the SUSD reports

--- R/plots.R
+++ R/plots.R
@@ -105,13 +105,14 @@ neami_waffle <- function(data, count_var, rows = NULL, palette = "Set2") {
   
   if(!is.null(rows)) {warning("The rows argument is deprecated.")}
   
-  smart_round <- function(x, digits = 0) { # somewhere on SO
-    up <- 10 ^ digits
-    x <- x * up
+  smart_round <- function(x#, digits = 0
+                          ) { # somewhere on SO
+    # up <- 10 ^ digits
+    # x <- x * up
     y <- floor(x)
     indices <- tail(order(x-y), round(sum(x)) - sum(y))
     y[indices] <- y[indices] + 1
-    y / up
+    y #/ up
   }
   
   data |> 
@@ -119,8 +120,8 @@ neami_waffle <- function(data, count_var, rows = NULL, palette = "Set2") {
     dplyr::group_by({{count_var}}) |>
     dplyr::summarise(count = dplyr::n()) |>
     dplyr::mutate({{count_var}} := as.factor({{count_var}}), 
-                  pct = count / sum(count), 
-                  pct = as.integer(smart_round(pct, 1) * 100L)) |> 
+                  pct = count / sum(count) * 100L, 
+                  pct = as.integer(smart_round(pct) )) |> 
     dplyr::select(-count) |> 
     tidyr::uncount(pct) |> 
     dplyr::bind_cols(expand_grid(y = 1:10, x = 1:10)) |> 
